028ce077b0d808de5ef4d596b051e5e3f17c6b78
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-17 10:16:51 +0900

    Create cart-delete-all.php

---
+++ cart-delete-all.php
@@ -0,0 +1,13 @@
+<?php require 'header.php'; ?>
+
+<?php
+unset($_SESSION['product']);
+unset($_SESSION['cart']);?>
+
+<div id="pan" class="clearfixed">
+<h2>カート</h2>
+<p>カートを空にしました。</p>
+<p><a href="index1.php">ホームへ戻る</a></p>
+</div>
+
+<?php require 'footer.php'; ?>
