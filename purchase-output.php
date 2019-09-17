d0c559587036d8a772bbccb8f9df24d9fc84c842
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-17 11:03:52 +0900

    Create purchase-output.php

---
+++ purchase-output.php
@@ -0,0 +1,37 @@
+<?php require 'header.php'; ?>
+<div id="pan" class="clearfixed">
+<div class="purchase">
+<?php
+$pdo=new PDO('mysql:host=localhost;dbname=shop;charset=utf8',
+	'staff', 'password');
+$purchase_id=1;
+foreach ($pdo->query('select max(id) from purchase') as $row) {
+	$purchase_id=$row['max(id)']+1;
+}
+$sql=$pdo->prepare('insert into purchase values(?,?,0)');
+if ($sql->execute([$purchase_id, $_SESSION['customer']['id']])) {
+	foreach ($_SESSION['product'] as $product_id=>$product) {
+		$sql=$pdo->prepare('insert into purchase_detail values(?,?,?,null)');
+		$sql->execute([$purchase_id, $product_id, $product['count']]);
+	}
+	unset($_SESSION['product']);
+	unset($_SESSION['cart']);
+	echo '<div class="purchase-message">';
+	echo '<div class="purchase-img">';
+	echo '<img src="images/main-logo2.png" hight="250px" width="250px">';
+	echo '</div>';
+	echo '<div class="thank">';
+	echo '<div class="bold">ご購入の手続きが完了しました。ご注文ありがとうございました。</div>';
+	echo '<p><a href="index1.php">ホームへ戻る</a></p>';
+	echo '</div>';
+	echo '</div>';
+
+} else {
+	echo '大変申し訳ございません。ご購入の手続き中にエラーが発生しました。';
+	echo 'もう一度やり直してください。';
+	echo '<p><a href="purchase-input.php">カートへ戻る</a></p>';
+}
+?>
+</div>
+</div>
+<?php require 'footer.php'; ?>
