63bbfd93cbe2a4d0f04b5eb68bb915ae88a2df47
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-17 10:49:30 +0900

    Create logout-output.php

---
+++ logout-output.php
@@ -0,0 +1,9 @@
+<?php require 'header.php'; ?>
+<?php
+if (isset($_SESSION['customer'])) {
+	unset($_SESSION['customer']);
+	echo '<div id="pan" class="clearfix main">ログアウトしました。</div>';
+} else {
+	echo '<div id="pan" class="clearfix main">すでにログアウトしています。</div>';
+}
+?>
