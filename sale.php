60c465589bab031fd9309cdc44e971e1c5256fcc
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-17 10:13:02 +0900

    Create sale.php

---
+++ sale.php
@@ -0,0 +1,39 @@
+
+<?php
+require_once(__DIR__.'/config.php');
+
+try{
+	$db=new PDO(DSN,DB_USERNAME,DB_PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+ }catch(PDOException $e){
+     echo $e->getmessage();
+     exit;
+ } 
+?>
+
+ <?php require 'header.php';?>
+ <main>
+<div id="pan" class="clearfix">
+<div class="category">
+		<a href="index1.php">all </a>/<a href="top-sales.php"> top-sales </a>/<a href="beer.php"> beer </a>/<a href="wine.php"> wine </a>/<a href="whisky.php"> whisky </a>/<a href="sale.php"> sale </a>
+	
+			<form action="search.php" method="post" class="index-search">
+				<input type="text" name="keyword">
+				<input type="submit" value="検索" class="button">
+			</form>
+	</div>
+	<div class="category_block">
+<?php
+$sql=$db->query("select * from sale_product ");
+foreach ($sql as $row) :?>
+<div class="category_kind">
+	<a href="sale-detail.php?id=<?php echo $row['product_id']; ?>">
+		<img src="images/<?php echo $row['product_id']; ?>.jpg" width="180" height="180" class="img_hover"></a>
+<p><?php echo '<div class="category_name">',$row['name'], '</div>'; ?></p>
+<p><?php echo round($row['price']*1.08); ?>円(税込）</p>
+</div>
+<?php endforeach;?>
+</div>
+</div>
+ </main>
+<?php require 'footer.php'?>	
