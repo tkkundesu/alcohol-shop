d4a65d1bc4a2edfb3f1a9bf42a61ff7a54cb6477
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-17 10:34:31 +0900

    Create customer-input.php

---
+++ customer-input.php
@@ -0,0 +1,21 @@
+
+<?php require 'header.php';?>
+<?php
+echo '<div id="pan" class="clearfix"><h2>新規登録</h2>';
+echo '<form action ="customer-confirm.php" method="post">';
+echo '<p>お名前<br>';
+echo '<span><input type="text" name="name" value="" class="textbox">';
+echo '</span></p>';
+echo '<p>ご住所<br>';
+echo '<span><input type="text" name="address" value="" class="textbox">';
+echo '</span></p>';
+echo '<p>ログイン名<br>';
+echo '<span><input type="text" name="login" value="" class="textbox">';
+echo '</span></p>';
+echo '<p>パスワード<br>';
+echo '<span><input type="password" name="password" value="" class="textbox">';
+echo '</span></p>';
+echo '<div class="b"><input type="submit" value="確定" class="button"></div>';
+echo '</form></div>';
+?>
+<?php require 'footer.php';?>
