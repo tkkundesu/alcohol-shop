31d360ed838789b93f5949bbed4b795f59c8f92e
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-17 10:45:08 +0900

    Create logincart-input.php

---
+++ logincart-input.php
@@ -0,0 +1,10 @@
+<h2>ログイン</h2>
+<form action="logincart-output.php" method="post">
+<p>ログイン名<br>
+	<span><input type="text" id="rog"name="login" class="textbox"></span></p>
+	<br>
+<p>パスワード<br>
+	<span><input type="password" id="rog"name="password" class="textbox"></span></p>
+	<div class="b"><input type="submit" value="ログイン" class="button"></div>
+</form>
+<?php require 'footer.php'?>
