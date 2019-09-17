4b11d4bf4b4fe7ac7769e29e4c99d8aa9522c318
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-17 10:48:40 +0900

    Create logout-input.php

---
+++ logout-input.php
@@ -0,0 +1,5 @@
+<?php require 'header.php'; ?>
+<div id="pan" class="clearfix"><p>ログアウトしますか？</p>
+<a href="logout-output.php"><input type="button"value="はい" class="button"></a>
+<a href="index1.php"><input type="button" value="いいえ" name="" class="button"></a></div>
+<?php require 'footer.php'?>
