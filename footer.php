545546e5b16400d3dd86c32f868556e0ca91a815
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-17 10:41:19 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+</div>	
+
+
+	<footer class="footer-clearfix">
+	<p>&copy;Copyright 2019酒太郎 ALL rights reserved.</p><span class="owner"><a href="owner-login.php" >管理者ページ</a></span>
+
+	</footer>
+
+</body>
+</html>
