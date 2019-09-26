4a236888ff92f2cdf68d44f23956aefc43c64d5a
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-26 18:09:38 +0900

    Update favorite-s.php

--- favorite-s.php
+++ favorite-s.php
@@ -1,5 +1,16 @@
 
 <?php require 'header.php';?>
+<?php
+require_once(__DIR__.'/config.php');
+
+try{
+	$pdo=new PDO(DSN,DB_USERNAME,DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+ }catch(PDOException $e){
+     echo $e->getmessage();
+     exit;
+ } 
+?>
  <main>
 <div id="pan" class="clearfix">	
 <div class="category">
@@ -14,8 +25,6 @@
 <?php
 if (isset($_SESSION['customer'])) {
 		
-	$pdo=new PDO('mysql:host=localhost;dbname=shop;charset=utf8', 
-		'staff', 'password');
 	$sql=$pdo->prepare(
 		'select * from favorite, product '.
 		'where customer_id=? and product_id=id');
