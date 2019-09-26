6fadc84a5828c2a9c11cd54cfbe0f7e21efc3b96
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-27 00:18:56 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 
-define('DSN','mysql:host=localhost;dbname=shop;charset=utf8;unix_socket=/tmp/mysql.sock');
+define('DSN','mysql:host=localhost;dbname=shop;charset=utf8;unix_socket=/var/mysql/mysql.sock');
 define('DB_USERNAME','staff');
 define('DB_PASSWORD','password');
 
