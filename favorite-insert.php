735502229e9ae825d9fba8e6bb58b2137cc8c622
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-26 18:10:20 +0900

    Update favorite-insert.php

--- favorite-insert.php
+++ favorite-insert.php
@@ -1,8 +1,18 @@
 <?php require 'header.php';?>
 <?php
+require_once(__DIR__.'/config.php');
+
+try{
+	$pdo=new PDO(DSN,DB_USERNAME,DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+ }catch(PDOException $e){
+     echo $e->getmessage();
+     exit;
+ } 
+?>
+<?php
 if (isset($_SESSION['customer'])) {
-	$pdo=new PDO('mysql:host=localhost;dbname=shop;charset=utf8', 
-		'staff', 'password');
+	
 	$sql=$pdo->prepare('insert into favorite values(?,?)');
 	$sql->execute([$_SESSION['customer']['id'], $_REQUEST['id']]);
 	echo '<a "favorite-insert.php">お気に入り</a>に商品を追加しました。';
