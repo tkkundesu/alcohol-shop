24f7726c2735466caa1c394eca8d776c73a425bd
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-27 15:51:06 +0900

    Update cart-insert.php

--- cart-insert.php
+++ cart-insert.php
@@ -1,14 +1,14 @@
 <?php require 'header.php'; ?>
 
 <?php
-$cart=$_SESSION['cart'];
+$cart=$_SESSION['cart'];//カートの個数を管理するセッション
 $id=$_REQUEST['id'];
 if(!isset($_SESSION['product'])){
-	$_SESSION['product']=[];
+	$_SESSION['product']=[];//カートの中身を管理するセッションが定義されていなかったら用意する
 }
-$count=0;
+$count=0;//カートの個数を管理するセッションの初期化
 if(isset($_SESSION['product'][$id])){
-	$count=$_SESSION['product'][$id]['count'];
+	$count=$_SESSION['product'][$id]['count'];//
 }
 $_SESSION['product'][$id]=[
 'name'=>$_REQUEST['name'],
