323957b60b9676386e150ec2be63ae7670a817c3
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-27 15:44:15 +0900

    Update cart-delete.php

--- cart-delete.php
+++ cart-delete.php
@@ -1,8 +1,9 @@
 <?php require 'header.php'; ?>
 
 <?php
+//カート内の全体の個数から任意の商品の個数を減らす
 $_SESSION['cart']['count']=$_SESSION['cart']['count']-$_SESSION['product'][$_REQUEST['id']]['count'];
-unset($_SESSION['product'][$_REQUEST['id']]);
+unset($_SESSION['product'][$_REQUEST['id']]);//カート内の任意の商品をアンセットする
 echo '<script>alert("カートから【',$_REQUEST['delete'],'】を削除しました。");</script>';
 require 'cart.php';
 
