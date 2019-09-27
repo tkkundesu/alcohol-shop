8af08e01e49008c84925d452e776a4c3c26f9a3e
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-28 00:37:44 +0900

    Update customer-output.php

--- customer-output.php
+++ customer-output.php
@@ -2,26 +2,26 @@
 <?php
 require_once(__DIR__.'/config.php');
 try{
-	$pdo=new PDO(DSN,DB_USERNAME,DB_PASSWORD);
+	$pdo=new PDO(DSN,DB_USERNAME,DB_PASSWORD);//データベース接続
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
  }catch(PDOException $e){
-     echo $e->getmessage();
+     echo $e->getmessage();//エラー表示
      exit;
  } 
 if (isset($_SESSION['customer'])) {
-	$id=$_SESSION['customer']['id'];
+	$id=$_SESSION['customer']['id'];//ログイン時、ログイン名重複チェック
 	$sql=$pdo->prepare('select * from customer where id!=? and login=?');
 	$sql->execute([$id,$_REQUEST['login']]);
 } else {
-	$sql=$pdo->prepare('select * from customer where login=?');
+	$sql=$pdo->prepare('select * from customer where login=?');//ログイン名重複チェック
 	$sql->execute([$_REQUEST['login']]);
 }
-if (empty($sql->fetchAll())) {
+if (empty($sql->fetchAll())) {//抽出したsql文が空である場合
 	if (isset($_SESSION['customer'])) {
 		echo '<div id="pan" class="clearfix main">すでにログインしています。<br>
 		<a href="index1.php">ホームに戻る</a></div>';
 	} else {
-	$sql=$pdo->prepare('insert into customer values(null,?,?,?,?)');
+	$sql=$pdo->prepare('insert into customer values(null,?,?,?,?)');//新規顧客情報をカスタマーテーブルへ挿入
 	$sql->execute([
 		$_REQUEST['name'],$_REQUEST['address'],$_REQUEST['login'],$_REQUEST['password']]);
 	echo '<div id="pan" class="clearfix main">お客様情報を登録しました。<br>
