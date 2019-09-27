49928ad8da1d55daa953b5d2e855561133b77b59
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-27 22:57:23 +0900

    Update purchase-output.php

--- purchase-output.php
+++ purchase-output.php
@@ -3,10 +3,10 @@
 require_once(__DIR__.'/config.php');
 
 try{
-	$pdo=new PDO(DSN,DB_USERNAME,DB_PASSWORD);
+	$pdo=new PDO(DSN,DB_USERNAME,DB_PASSWORD);//データベース接続
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
  }catch(PDOException $e){
-     echo $e->getmessage();
+     echo $e->getmessage();//エラー表示
      exit;
  } 
 ?>
@@ -14,17 +14,17 @@ try{
 <div class="purchase">
 <?php
 $purchase_id=1;
-foreach ($pdo->query('select max(id) from purchase') as $row) {
-	$purchase_id=$row['max(id)']+1;
+foreach ($pdo->query('select max(id) from purchase') as $row) {//現在のpurchaseテーブルの最大のID番号の情報を抽出
+	$purchase_id=$row['max(id)']+1;//抽出したID番号に1足した値が今回追加するIDとなる
 }
-$sql=$pdo->prepare('insert into purchase values(?,?,0)');
+$sql=$pdo->prepare('insert into purchase values(?,?,0)');//purchaseテーブルに追加
 if ($sql->execute([$purchase_id, $_SESSION['customer']['id']])) {
-	foreach ($_SESSION['product'] as $product_id=>$product) {
+	foreach ($_SESSION['product'] as $product_id=>$product) {//セッションプロダクトに入っている情報をpurchase detailテーブルに追加
 		$sql=$pdo->prepare('insert into purchase_detail values(?,?,?,null)');
 		$sql->execute([$purchase_id, $product_id, $product['count']]);
 	}
-	unset($_SESSION['product']);
-	unset($_SESSION['cart']);
+	unset($_SESSION['product']);//カート内セッションのアンセット
+	unset($_SESSION['cart']);//カート内の個数のセッションのアンセット
 	echo '<div class="purchase-message">';
 	echo '<div class="purchase-img">';
 	echo '<img src="images/main-logo2.png" hight="250px" width="250px">';
