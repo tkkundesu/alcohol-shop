bcc9c73c63a8e3b850f1b2a872ded18a4d334082
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-27 19:00:29 +0900

    Update cart-insert.php

--- cart-insert.php
+++ cart-insert.php
@@ -8,14 +8,14 @@ if(!isset($_SESSION['product'])){
 }
 $count=0;//カートの個数を管理するセッションの初期化
 if(isset($_SESSION['product'][$id])){
-	$count=$_SESSION['product'][$id]['count'];//
+	$count=$_SESSION['product'][$id]['count'];//任意の商品がすでに入っている場合個数を変数に代入
 }
-$_SESSION['product'][$id]=[
+$_SESSION['product'][$id]=[//セッションに格納
 'name'=>$_REQUEST['name'],
 'price'=>$_REQUEST['price'],
 'count'=>$count+$_REQUEST['count']
 ];
-$cart=$_SESSION['product']['count']+$cart;
+$cart=$_SESSION['product']['count']+$cart;//カートの個数に追加
 echo '<script>alert("カートに【',$_REQUEST['name'],'】を追加しました");</script>';
 
 
