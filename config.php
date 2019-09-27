bbc2d95e3405bf624a8f7b4e10f5dc12a1e8a12c
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-27 12:24:11 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 ini_set('display_errors', 1);
 
-define('DSN','mysql:host=localhost;dbname=shop;charset=utf8;unix_socket=/var/mysql/mysql.sock');
-define('DB_USERNAME','staff');
-define('DB_PASSWORD','password');
+define('DSN','mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_8744cea104e5bbe;charset=utf8;unix_socket=/var/mysql/mysql.sock');
+define('DB_USERNAME','b5b5250bdc1f04');
+define('DB_PASSWORD','ecaa3b1d');
 
 ?>
