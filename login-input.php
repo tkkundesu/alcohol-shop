187faa9e694ea8dacbd31ee202efb9872310a3db
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-30 09:39:12 +0900

    Update login-input.php

--- login-input.php
+++ login-input.php
@@ -3,27 +3,27 @@
 require_once(__DIR__.'/config.php');
 
 try{
-	$pdo=new PDO(DSN,DB_USERNAME,DB_PASSWORD);
+	$pdo=new PDO(DSN,DB_USERNAME,DB_PASSWORD);//データベース接続
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
  }catch(PDOException $e){
-     echo $e->getmessage();
+     echo $e->getmessage();//エラー表示
      exit;
  } 
 ?>
 <?php
  if($_SERVER['REQUEST_METHOD']==='POST'){
-unset($_SESSION['customer']);
+unset($_SESSION['customer']);//カスタマーセッションアンセット
 
-$sql=$pdo->prepare('select * from customer where login=? and password=?');
+$sql=$pdo->prepare('select * from customer where login=? and password=?');//送られてきた情報から抽出
 $sql->execute([$_REQUEST['login'], $_REQUEST['password']]);
 foreach ($sql as $row) {
-	$_SESSION['customer']=[
+	$_SESSION['customer']=[//セッションに格納
 		'id'=>$row['id'], 'name'=>$row['name'], 
 		'address'=>$row['address'], 'login'=>$row['login'], 
 		'password'=>$row['password']];
 }
 if (isset($_SESSION['customer'])) {
-	header('Location:index1.php');exit();
+	header('Location:index1.php');exit();//リダイレクト
 } else {
 	echo '<script>alert("ログイン名またはパスワードが違います。");</script>';
 }
