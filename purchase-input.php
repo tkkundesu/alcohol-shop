d436ed98a445309e632a74587bf7e75c43a25f4e
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-30 11:24:00 +0900

    Update purchase-input.php

--- purchase-input.php
+++ purchase-input.php
@@ -3,21 +3,21 @@
 
 <?php
 
-if (!isset($_SESSION['customer'])) {
+if (!isset($_SESSION['customer'])) {//ログインしていないとき
 	echo '<script>alert("ご購入手続きにはログインが必要です。")</script>';
 	require 'logincart-input.php';
 } else
-if (empty($_SESSION['product'])) {
+if (empty($_SESSION['product'])) {//カート空のとき
 	echo '<h2>ご注文内容</h2>';
 	echo 'カートに商品がありません。';
 } else {
 	echo '<h2>ご注文内容</h2>';
-	echo '<p class="bold">ー お届け先 ー</p>';
+	echo '<p class="bold">ー お届け先 ー</p>';//顧客名、住所表示
 	echo '<p>お名前：', $_SESSION['customer']['name'], '</p>';
 	echo '<p>ご住所：', $_SESSION['customer']['address'], '</p>';
 
 	echo '<div class="require-cart">';
-	if(!empty($_SESSION['product'])){
+	if(!empty($_SESSION['product'])){//カートの商品が入っているとき詳細表示
 		echo '<table class="table-form">';
 		echo '<th></th><th class="num">商品番号</th><th>商品名</th><th>本体価格</th><th>税込価格</th><th>数量</th><th>小計</th><th></th>';
 		$total=0;
@@ -34,7 +34,7 @@ if (empty($_SESSION['product'])) {
 			$subtotal=$includeTax*$product['count'];
 			$total+=$includeTax;
 			echo '<td>',round($subtotal),'円</td>';
-			echo '<form class="button" action="cart-delete.php">';
+			echo '<form class="button" action="cart-delete.php">';//削除処理
 			echo '<input type="hidden" name="id" value=',$id,'>';
 			echo '<input type="hidden" name="delete" value=',$product['name'],'>';
 			echo '<td><input class="button" type="submit" value="削除"></td>';
