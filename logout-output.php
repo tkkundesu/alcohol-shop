85443f15cc470d1e636b38b102e9f5741c23f1e8
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-30 09:39:51 +0900

    Update logout-output.php

--- logout-output.php
+++ logout-output.php
@@ -1,6 +1,6 @@
 <?php require 'header.php'; ?>
 <?php
-if (isset($_SESSION['customer'])) {
+if (isset($_SESSION['customer'])) {//ログイン状態の時はアンセットする
 	unset($_SESSION['customer']);
 	echo '<div id="pan" class="clearfix main">ログアウトしました。</div>';
 } else {
