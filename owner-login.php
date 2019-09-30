efbe759b6361ab401a042a993924db4ed5f0112c
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-30 09:47:42 +0900

    Update owner-login.php

--- owner-login.php
+++ owner-login.php
@@ -2,19 +2,19 @@
 <?php
 require_once(__DIR__.'/config.php');
 try{
-	$db=new PDO(DSN,DB_USERNAME,DB_PASSWORD);
+	$db=new PDO(DSN,DB_USERNAME,DB_PASSWORD);//データベース接続
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
  }catch(PDOException $e){
-     echo $e->getmessage();
+     echo $e->getmessage();//エラー表示
      exit;
  } 
    if($_SERVER['REQUEST_METHOD']==='POST'){
-   	unset($_SESSION["owner"]);
-    if(!$_REQUEST["login"]=='' && !$_REQUEST["password"]==''){
-   	$sql=$db->prepare('select * from owner where owner_login=? and owner_password=?');
+   	unset($_SESSION["owner"]);//アンセット
+    if(!$_REQUEST["login"]=='' && !$_REQUEST["password"]==''){//空欄でない場合
+   	$sql=$db->prepare('select * from owner where owner_login=? and owner_password=?');//送られた情報から抽出
     $sql->execute([$_REQUEST["login"],$_REQUEST["password"]]);
     foreach ($sql as $row) {
-      $_SESSION["owner"]=[
+      $_SESSION["owner"]=[//セッション格納
         'login'=>$row["owner_login"],'password'=>$row["owner_password"]
       ];
    	}
@@ -24,7 +24,7 @@ try{
     exit();
    }
  
-   if(isset($_SESSION["owner"])){
+   if(isset($_SESSION["owner"])){//セッションに格納できたときリダイレクト
    	header('Location:owner.php');exit();
    	}else{
     echo '<script>alert("ログイン名またはパスワードが違います。");</script>';
