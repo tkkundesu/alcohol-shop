d62d6298eb939e3799a5ef6f1c94b5dea326faef
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-30 11:42:29 +0900

    Update top-sales.php

--- top-sales.php
+++ top-sales.php
@@ -3,10 +3,10 @@
 require_once(__DIR__.'/config.php');
 
 try{
-	$db=new PDO(DSN,DB_USERNAME,DB_PASSWORD);
+	$db=new PDO(DSN,DB_USERNAME,DB_PASSWORD);//データベース接続
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
  }catch(PDOException $e){
-     echo $e->getmessage();
+     echo $e->getmessage();//エラー表示
      exit;
  } 
 
@@ -27,7 +27,7 @@ try{
 	<h2>今売れている商品の一覧</h2>
 	<div class="category_block">
 		<?php
-		$sql=$db->query("select product_id,price,name from purchase_detail,product where product_id=product.id group by product_id order by sum(count) DESC ");
+		$sql=$db->query("select product_id,price,name from purchase_detail,product where product_id=product.id group by product_id order by sum(count) DESC ");//購入個数が多い順番に抽出
 		foreach ($sql as $row) :?>
 		<div class="category_kind"><a href="detail.php?id=<?php echo $row['product_id']; ?>"><img src="images/<?php echo $row['product_id']; ?>.jpg" width="180" height="180" class="img_hover"></a>
 		<p><div class="category_name"><?php echo $row['name']; ?></div></p>
